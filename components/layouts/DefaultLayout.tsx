3ec2495bcae820a9f564308a2f4991c0dad5ad15
Ahmet <rganned@gmail.com>
2022-10-10 13:32:27 +0300

    add: information text about Twitch API.

--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -9,6 +9,11 @@ type DefaultLayoutProps = {
 const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      <div className="py-2.5 px-1.5 bg-red-900 text-white text-sm text-center font-bold">
+        <span>
+          The Twitch endpoint we&apos;re using will not be public soon.
+        </span>
+      </div>
       <Header />
       <main className="flex flex-col flex-1">{children}</main>
       <Footer />
