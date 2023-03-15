31cee73c80645de7f3a32e27c5a6bb0bd10c40e1
Ahmet <rganned@gmail.com>
2023-03-15 11:31:34 +0300

    updated deprecation notice

--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import BuyMeACoffee from "components/shared/BuyMeACoffee";
 import Footer from "components/shared/Footer";
 import Header from "components/shared/Header";
+import Link from "next/link";
 
 type DefaultLayoutProps = {
   children: React.ReactNode;
@@ -9,15 +10,31 @@ type DefaultLayoutProps = {
 const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="py-2.5 px-1.5 bg-red-900 text-white text-sm text-center font-bold">
-        <span>
-          The Twitch endpoint we&apos;re using will not be public soon.
-        </span>
-      </div>
+      <DeprecationNotice />
       <Header />
       <main className="flex flex-col flex-1">{children}</main>
       <Footer />
       <BuyMeACoffee />
+    </div>
+  );
+};
+
+const DeprecationNotice = () => {
+  return (
+    <div className="py-2.5 px-1.5 bg-red-900 text-white text-sm text-center font-bold">
+      <span>
+        Important: Due to Twitch&apos;s termination of the endpoint we&apos;re
+        using, Twitch Followings will no longer be available as of{" "}
+        <Link
+          className="underline"
+          href="https://discuss.dev.twitch.tv/t/follows-endpoints-and-eventsub-subscription-type-are-now-available-in-open-beta/43322"
+          target="_blank"
+          rel="noreferrer"
+        >
+          August 3, 2023
+        </Link>
+        .
+      </span>
     </div>
   );
 };
